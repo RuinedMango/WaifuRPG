9806226df43a39919600ba5aba3743b1b3be4c0f
RuinedMango <00rudystx@gmail.com>
2024-03-17 01:42:51 -0800

    Cmake cmade

---
+++ cmake-build-debug-cygwin/CMakeFiles/WaifuRPG.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WaifuRPG.dir/src/main.cpp.o"
+  "CMakeFiles/WaifuRPG.dir/src/main.cpp.o.d"
+  "WaifuRPG.exe"
+  "WaifuRPG.pdb"
+  "libWaifuRPG.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WaifuRPG.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
