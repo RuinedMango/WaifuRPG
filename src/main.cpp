193db5949abbaf05e23da74826cd9f9a354896d2
RuinedMango <00rudystx@gmail.com>
2024-03-19 17:39:17 -0800

    Added Framebuffers

--- src/main.cpp
+++ src/main.cpp
@@ -84,6 +84,7 @@ private:
 	VkExtent2D swapChainExtent;
 	std::vector<VkImageView> swapChainImageViews;
 
+	std::vector<VkFramebuffer> swapChainFramebuffers;
 	VkRenderPass renderPass;
 	VkPipelineLayout pipelineLayout;
 	void initWindow(){
@@ -106,6 +107,7 @@ private:
 		createImageViews();
 		createRenderPass();
 		createGraphicsPipeline();
+		createFramebuffers();
 	}
 	void mainLoop(){
 		while(!glfwWindowShouldClose(window)){
@@ -113,6 +115,9 @@ private:
 		}
 	}
 	void cleanup(){
+		for(auto framebuffer : swapChainFramebuffers) {
+			vkDestroyFramebuffer(device, framebuffer, nullptr);
+		}
 		vkDestroyPipeline(device, graphicsPipeline, nullptr);
 		vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
 		vkDestroyRenderPass(device, renderPass, nullptr);
@@ -135,6 +140,26 @@ private:
 	void createSurface(){
 		if(glfwCreateWindowSurface(instance, window, nullptr, &surface) != VK_SUCCESS){
 			throw std::runtime_error("Failed to create window surface");
+		}
+	}
+	void createFramebuffers() {
+		swapChainFramebuffers.resize(swapChainImageViews.size());
+		for(size_t i = 0; i < swapChainImageViews.size(); i++) {
+			VkImageView attachments[] = {
+				swapChainImageViews[i]
+			};
+			VkFramebufferCreateInfo framebufferInfo{};
+			framebufferInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+			framebufferInfo.renderPass = renderPass;
+			framebufferInfo.attachmentCount = 1;
+			framebufferInfo.pAttachments = attachments;
+			framebufferInfo.width = swapChainExtent.width;
+			framebufferInfo.height = swapChainExtent.height;
+			framebufferInfo.layers = 1;
+
+			if(vkCreateFramebuffer(device, &framebufferInfo, nullptr, &swapChainFramebuffers[i]) != VK_SUCCESS) {
+				throw std::runtime_error("failed to create framebuffer");
+			}
 		}
 	}
 	void createRenderPass() {
