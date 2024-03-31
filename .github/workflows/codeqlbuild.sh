60439545b4845cc45b9df419989681778fa64f03
RuinedMango <00rudystx@gmail.com>
2024-03-30 21:58:31 -0800

    working on codeql

--- .github/workflows/codeqlbuild.sh
+++ .github/workflows/codeqlbuild.sh
@@ -1,2 +1,2 @@
-cmake -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles" -S \ -B \cmake-build-debug
-cmake --build \cmake-build-debug --target WaifuRPG -- -j 10
+cmake -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles" -S \ -B cmake-build-debug
+cmake --build cmake-build-debug --target WaifuRPG -- -j 10
